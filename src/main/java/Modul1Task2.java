df3f62122f2ab54967f8331857bb2bf4ecaa39f8
Anna Kat <annasobolevskaya2017@gmail.com>
2023-06-05 21:33:26 +0300

    task2

--- src/main/java/Modul1Task2.java
+++ src/main/java/Modul1Task2.java
@@ -3,7 +3,7 @@ import java.util.Map;
 
 public class Modul1Task2 {
     public static void main(String[] args) {
-        int[] arr = {4,5,6,6,8};
+        int[] arr = {4,5,6,6,8,6,8};
         System.out.println(duplicateDetected(arr));
     }
     public static boolean duplicateDetected (int[] array) {
@@ -18,18 +18,15 @@ public class Modul1Task2 {
               duplicateMap.put(array[i], ++value);
            }
         }
-        HashMap<Integer, Integer> newMap = new HashMap<>();
-       for(Map.Entry <Integer, Integer> number : duplicateMap.entrySet()) {
-           if(!(number.getValue() == 1)) {
-               newMap.put(number.getKey(), number.getValue());
-           }
-       }
-        if(newMap.isEmpty()) {
+
+        duplicateMap.entrySet().removeIf(entry -> entry.getValue() == 1);
+
+        if(duplicateMap.isEmpty()) {
             result = false;
             System.out.println("Дубликатов нет");
         } else {
             result = true;
-            for(Map.Entry <Integer, Integer> number : newMap.entrySet()) {
+            for(Map.Entry<Integer, Integer> number : duplicateMap.entrySet()) {
                 System.out.println("Число " + number.getKey() + " повторяется " + number.getValue() + " раз");
             }
         }
